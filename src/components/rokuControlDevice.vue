af902b7dc0a67eb2ff9c127e9051aa2c404770b9
Andreslondono <andrescamilolondonomora@gmail.com>
2023-11-21 11:12:12 -0500

    correcion de la vista

--- src/components/rokuControlDevice.vue
+++ src/components/rokuControlDevice.vue
@@ -1,104 +1,61 @@
 <template>
-    <v-card
-    class="mx-auto my-8"
-    max-width="344"
-    elevation="07"
-  >
-  <v-row>
-    <v-col cols="2" class="d-flex align-center justify-center">
-        <v-icon icon="mdi-remote"/>
-    </v-col>
-    <v-col cols="8" class="container-title-device pl-0">
-        <v-row class="ma-0 pa-0">
-            <v-col cols="3" class="title-device">
-                <p class="font-weight-bold text-sm">Roku</p> 
-            </v-col>
-            <v-col  class="title-device-description">
-                <v-card-subtitle class="pl-0"> Univision </v-card-subtitle>
-            </v-col>
-        </v-row>
-        <v-row class="ma-0 pa-0">
-            <v-col cols="2" class="my-col pt-0">
-                <v-icon icon="mdi-circle" color="success"/>
-            </v-col>
-            <v-col class="d-flex align-center justify-start pl-0 pt-0">
-                <v-card-subtitle class="pl-0 icon-sm" > Online </v-card-subtitle>
-            </v-col>
-        </v-row>
-    </v-col>
-    <v-col cols="2" class="d-flex align-center justify-center">
-        <v-btn
-            class="ma-2"
-            color="primary"
-            :disabled="dialog"
-            :loading="dialog"
-            @click="dialog = true"
-        >
-        <v-icon icon="mdi-power"/>
-      </v-btn>
-      <v-dialog
-        v-model="dialog"
-        :scrim="false"
-        persistent
-        width="auto"
-      >
-      <v-card
-        color="primary"
-      >
-        <v-card-text>
-          Reiniciando Streaming
-          <v-progress-linear
-            indeterminate
-            color="white"
-            class="mb-0"
-          ></v-progress-linear>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+  <v-card style="overflow: initial; z-index: initial">
+    <v-card-body class="v-row">
+      <div class="v-col-md-2 v-col-2 text-center">
+        <i class="bi bi-speaker-fill" style="font-size: 2rem; color: cornflowerblue;"></i>
+      </div>
+      <div class="v-col-md-8 v-col-8">
+        <div class="text-left">
+            <strong>Module 7</strong> <i class="text-subtitle-2">Speaker</i>
+        </div>
+        <div class="text-left">
+          <h6><i class="bi bi-circle" style="color: rgb(55, 185, 4);"></i> Online</h6> 
+        </div>
+      </div>
 
-       
-        
-    </v-col>
-  </v-row>
-
-  </v-card>
-  
+      <div class="v-col-md-2 v-col-2 text-center">
+        <v-btn icon="$vuetify" size="x-small">
+            <i class="bi bi-power" style="font-size: 2rem;"></i>
+        </v-btn>
+      </div>
+    </v-card-body>
+  </v-card>    
 </template>
 
-<script >
-import createVuetify from '../plugins/vuetify';
-import { ref, watch, onMounted } from 'vue';
+<script>
+  import createVuetify from '../plugins/vuetify';
+  import { ref, watch, onMounted } from 'vue';
 
-export default {
-  setup() {
-    const dialog = ref(false);
+  export default {
+    setup() {
+      const dialog = ref(false);
 
-    watch(dialog, (val) => {
-      if (!val) return;
-      setTimeout(() => (dialog.value = false), 4000);
-    });
+      watch(dialog, (val) => {
+        if (!val) return;
+        setTimeout(() => (dialog.value = false), 4000);
+      });
 
-    return {
-      dialog,
-    };
-  },
-};
-</script>
-<style>
+      return {
+        dialog,
+      };
+    },
+  };
+  </script>
+  <style>
 
-.title-device{
-    padding-left: 0px;
-    padding-right: 0px;
-    padding-top: 15px;
-    padding-bottom: 0px;
-}
-.title-device-description{
-    padding-left: 0px;
-    padding-right: 0px;
-    padding-top: 15px;
-    padding-bottom: 0px;
-}
-.my-col {
-  padding: 10px 5px 10px 5px;;
-}
+  .title-device{
+      padding-left: 0px;
+      padding-right: 0px;
+      padding-top: 15px;
+      padding-bottom: 0px;
+  }
+  .title-device-description{
+      padding-left: 0px;
+      padding-right: 0px;
+      padding-top: 15px;
+      padding-bottom: 0px;
+  }
+  .my-col {
+    padding: 10px 5px 10px 5px;;
+  }
 </style>
--- src/views/DevicesView.vue
+++ src/views/DevicesView.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-import RokuControl from '../components/rokuControlDevice.vue'
+  import RokuControl from '../components/rokuControlDevice.vue'
 </script>
-
 <template>
-  <div>
-    <RokuControl/>
-    <RokuControl/>
-    <RokuControl/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  <div class="v-container">
+    <div class="v-row">
+      <div class="v-col-md-2 v-col-2"></div>
+      <div class="v-col-md-8 v-col-8">
+        <div class="mb-8">
+          <RokuControl/>
+        </div>
+        <div class="mb-8">
+          <RokuControl/>
+        </div>
+        <div class="mb-8">
+          <RokuControl/>
+        </div>
+      </div>
+      <div class="v-col-md-2 v-col-2"></div>
+    </div>
   </div>
 </template>
